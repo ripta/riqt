238fae916603ef8ab46ea7cbac9bf9280e89dfe7
Ripta Pasay <rpasay@rent.com>
2013-05-29 14:03:44 -0700

    NO ISSUE: move set/show registration to Rent::PIQT::Config
    Signed-off-by: Ripta Pasay <rpasay@rent.com>

--- lib/Rent/PIQT/Config.pm
+++ lib/Rent/PIQT/Config.pm
@@ -1,38 +1,94 @@
 package Rent::PIQT::Config;
 
-use Moo::Role;
+use Moo;
 
 with 'Rent::PIQT::Component';
-
-requires qw/load save/;
-
 
 sub AUTOLOAD {
     my ($self, @args) = @_;
 
     our $AUTOLOAD;
-    my $name = $AUTOLOAD;
-    $name =~ s/.*:://;
+    my $name = lc $AUTOLOAD;    # Normalize the method name by lower-casing it,
+    $name =~ s/.*:://;          # and removing its package
 
-    $self->{'_'} ||= { };
+    $self->{'kv'} ||= { };
 
     if (scalar(@args) == 0) {
-        return $self->{'_'}->{$name} && ref($self->{'_'}->{$name}) eq 'ARRAY'
-            ? wantarray
-                ? @{$self->{'_'}->{$name}}
-                : $self->{'_'}->{$name}->[0]
-            : wantarray
-                ? ()
-                : undef;
-    } else {
-        if ($self->controller) {
-            $self->controller->output->debug("SET $name " . join(' ', @args));
+        return exists($self->{'kv'}->{$name}) ? $self->{'kv'}->{$name} : undef;
+    } elsif (scalar(@args) == 1) {
+        my $value = $args[0];
+        if (uc $value eq 'ON') {
+            $value = 1;
+        } elsif (uc $value eq 'OFF') {
+            $value = 0;
         }
 
-        $self->{'_'}->{$name} = \@args;
+        $self->{'kv'}->{$name} = $value;
         return 1;
+    } else {
+        $self->controller->output->errorf(
+            "Multiple (%d) values were provided for configuration parameter '%s'",
+            scalar(@args),
+            $name,
+        );
     }
 }
 
+sub KEYS {
+    my ($self) = @_;
+    die ref($self) . "->KEYS must be called in array context" unless wantarray;
+    return keys %{$self->{'kv'}} if $self->{'kv'};
+    return ();
+}
+
+sub POSTBUILD {
+    my ($self) = @_;
+
+    $self->controller->register('set',
+        sub {
+            my ($self, $name, $value) = @_;
+            if (defined $value) {
+                $name = lc $name;
+                $name =~ s/\s/_/g;
+
+                $self->output->info("SET $name $value");
+                $self->config->$name($value);
+            } else {
+                $self->output->errorf("'SET %s' requires a value", $name);
+            }
+
+            return 1;
+        }
+    );
+
+    $self->controller->register('show',
+        sub {
+            my ($self, $name) = @_;
+            my $c = $self->config;
+            my $o = $self->output;
+
+            $o->start(
+                [
+                    {name => "Name",  type => "string", length => 255},
+                    {name => "Value", type => "string", length => 4000},
+                ]
+            );
+            if (defined $name) {
+                if (my $value = $c->$name) {
+                    $o->record([$name, $value]);
+                } else {
+                    $o->record([$name, undef]);
+                }
+            } else {
+                foreach my $key (sort $c->KEYS) {
+                    $o->record([$key, $c->$key]);
+                }
+            }
+            $o->finish;
+
+            return 1;
+        }
+    );
+}
 
 1;
--- lib/Rent/PIQT/Config/File.pm
+++ lib/Rent/PIQT/Config/File.pm
@@ -2,22 +2,74 @@ package Rent::PIQT::Config::File;
 
 use Moo;
 
-with 'Rent::PIQT::Config';
+extends 'Rent::PIQT::Config';
 
 has 'filename' => (is => 'rw', required => 1);
 
 sub BUILDARGS {
     my ($class, $filename) = @_;
+
+    # Trust the filename if the file already exists as-is, or if it's an
+    # absolute pathname
+    return { filename => $filename } if -e $filename;
+    return { filename => $filename } if $filename =~ m#^/#;
+
+    # Expand ~ to the home directory if one exists
+    return { filename => $filename } if $filename =~ s#~/#$ENV{'HOME'} . '/'#e && defined($ENV{'HOME'});
+
+    # Relative names should be constructed against the home directory
+    return { filename => $ENV{'HOME'} . '/' . $filename } if defined($ENV{'HOME'});
+    return { filename => "/home/" . $ENV{'LOGNAME'} . "/" . $filename } if defined($ENV{'LOGNAME'});
+    return { filename => "/home/" . $ENV{'USER'} . "/" . $filename } if defined($ENV{'USER'});
+
+    # Last-resort fallback
     return {
         filename => $filename
     };
 }
 
-sub load {
+sub DEMOLISH {
+    my ($self) = @_;
+    foreach my $key (keys %{$self->{'kv'}}) {
+        $self->controller->output->debugf("SET %s %s", $key, $self->{'kv'}->{$key});
+    }
 }
 
-sub save {
-}
+around POSTBUILD => sub {
+    my ($orig, $self) = (shift, shift);
+    $self->$orig(@_);
+
+    unless (-e $self->filename) {
+        $self->controller->output->warnf("Configuration file '%s' does not exist.", $self->filename);
+        $self->controller->output->warnf("One will be created automatically upon exiting.");
+        return;
+    }
+
+    open(my $fh, $self->filename) or do {
+        $self->controller->output->errorf("Cannot open '%s': %s", $self->filename, $!);
+        return;
+    };
+
+    my $lineno = 0;
+    while (my $line = <$fh>) {
+        $lineno++;
+        chomp $line;
+
+        next if $line =~ m{^\s*#};
+
+        if ($line =~ m{^\s*SET\s+(\S+)\s+(.+?)(#.*)?$}i) {
+            $self->$1($2);
+            next;
+        }
+
+        $self->controller->output->warnf(
+            "Unknown command '%s' in %s line %d",
+            $line,
+            $self->filename,
+            $lineno,
+        );
+    }
+};
 
 
 1;
