3dc2e01a534c211aefa0ff9ed46e8a901ae51994
Ripta Pasay <rpasay@rent.com>
2013-06-03 11:23:58 -0700

    NO ISSUE: describe object plugin
    Signed-off-by: Ripta Pasay <rpasay@rent.com>

--- lib/Rent/PIQT/Plugin/DescribeObject.pm
+++ lib/Rent/PIQT/Plugin/DescribeObject.pm
@@ -10,7 +10,15 @@ sub BUILD {
     $self->controller->register('desc', 'describe',
         sub {
             my ($ctrl, $object_name) = @_;
-            $ctrl->db->describe_object($object_name);
+            my @infos = $ctrl->db->describe_object($object_name);
+            $ctrl->output->data_set(
+                [
+                    {name => 'Column Name', type => 'str', length => max(11, map { length $_->{'name'} } @infos)},
+                    {name => 'Type',        type => 'str', length => max( 4, map { length $_->{'type'} . $_->{'precision_scale'} } @infos)},
+                    {name => 'Nullable',    type => 'str', length => max( 8, map { length $_->{'null'} } @infos)},
+                ],
+                map { [ @{$_}{qw/name type null/} ] } @infos,
+            );
             return 1;
         },
     );
