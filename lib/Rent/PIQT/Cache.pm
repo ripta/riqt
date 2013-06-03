f016c0642507ea2daf1b64f1b764cf5fbd2b693c
Ripta Pasay <rpasay@rent.com>
2013-06-03 11:20:13 -0700

    NO ISSUE: implement file-based caching
    Signed-off-by: Ripta Pasay <rpasay@rent.com>

--- lib/Rent/PIQT/Cache.pm
+++ lib/Rent/PIQT/Cache.pm
@@ -1,27 +1,86 @@
 package Rent::PIQT::Cache;
 
-use Moo::Role;
+use Moo;
 
 with 'Rent::PIQT::Component';
 
+has 'namespace' => (is => 'rw', required => 0);
+
+sub _build_key {
+    my ($self, $key) = @_;
+    return '' unless $key;
+
+    return $key if $key =~ m#^/#;
+    return '/' . $key unless $self->namespace;
+
+    return $self->namespace . '/' . $key;
+}
+
+sub BUILD {
+    my ($self) = @_;
+    $self->{'kv'} ||= {};
+    # print "Loaded Cache (", join(', ', keys %{$self->{'kv'}}), ")\n";
+}
+
+sub POSTBUILD {
+    my ($self) = @_;
+
+    $self->namespace($self->controller->db->dsn);
+    $self->controller->register('load', 'load;',
+        sub {
+            my ($ctrl) = @_;
+            $ctrl->output->info("Caching object names for tab completion...");
+
+            if (my $objects = $ctrl->db->object_names) {
+                $ctrl->cache->set('object_names', $objects);
+                $ctrl->cache->set('object_ts',    time);
+                $ctrl->output->okf("Loaded %d objects into cache", scalar(@{ $ctrl->cache->get('object_names') }));
+            } else {
+                $ctrl->output->errorf("Could not load objects: %s",
+                    $ctrl->db->last_error || 'unknown error',
+                );
+            }
+
+            return 1;
+        },
+    );
+}
 
 # delete($self, $key)
-sub delete { return; }
+sub delete {
+    my ($self, $key) = @_;
+    $key = $self->_build_key($key);
+    # $self->controller->output->ok("CACHE DELETE $key");
+    delete $self->{'kv'}->{$key};
+}
 
 # get($self, $key)
-sub get { return; }
+sub get {
+    my ($self, $key) = @_;
+    $key = $self->_build_key($key);
+    # $self->controller->output->ok("CACHE GET $key");
+    return exists($self->{'kv'}->{$key}) ? $self->{'kv'}->{$key} : undef;
+}
+
+# save
+sub save {
+    my ($self) = @_;
+}
 
 # set($self, $key, $value)
-sub set { return; }
-
-# load()
-sub load { return; }
-
-# save()
-sub save { return; }
+sub set {
+    my ($self, $key, $value) = @_;
+    $key = $self->_build_key($key);
+    # $self->controller->output->ok("CACHE SET $key $value");
+    return $self->{'kv'}->{$key} = $value;
+}
 
 # touch()
-sub touch { return; }
+sub touch {
+    my ($self) = @_;
+    # $self->controller->output->ok("CACHE TOUCH");
+    return $self->{'kv'}->{'_touched'} = time;
+}
 
 
 1;
--- lib/Rent/PIQT/Cache/File.pm
+++ lib/Rent/PIQT/Cache/File.pm
@@ -1,17 +1,57 @@
 package Rent::PIQT::Cache::File;
 
 use Moo;
+use Storable qw/nstore retrieve/;
 
-with 'Rent::PIQT::Cache';
+extends 'Rent::PIQT::Cache';
 
 has 'filename' => (is => 'rw', required => 1);
 
+sub BUILD {
+    my ($self) = @_;
+
+    if (-e $self->filename) {
+        $self->{'kv'} = eval { retrieve $self->filename };
+        if ($@) {
+            warn sprintf("Cannot load cache from %s: %s\n",
+                $self->filename,
+                $@,
+            );
+            $self->{'kv'} = {};
+        }
+    } else {
+        $self->{'kv'} = {};
+    }
+    # print "Loaded Cache::File (", join(', ', keys %{$self->{'kv'}}), ")\n";
+};
+
 sub BUILDARGS {
     my ($class, $filename) = @_;
+
+    # Trust the filename if the file already exists as-is, or if it's an
+    # absolute pathname
+    return { filename => $filename } if -e $filename;
+    return { filename => $filename } if $filename =~ m#^/#;
+
+    # Expand ~ to the home directory if one exists
+    return { filename => $filename } if $filename =~ s#~/#$ENV{'HOME'} . '/'#e && defined($ENV{'HOME'});
+
+    # Relative names should be constructed against the home directory
+    return { filename => $ENV{'HOME'} . '/' . $filename } if defined($ENV{'HOME'});
+    return { filename => "/home/" . $ENV{'LOGNAME'} . "/" . $filename } if defined($ENV{'LOGNAME'});
+    return { filename => "/home/" . $ENV{'USER'} . "/" . $filename } if defined($ENV{'USER'});
+
+    # Last-resort fallback
     return {
         filename => $filename
     };
 }
+
+sub save {
+    my ($self) = @_;
+    nstore $self->{'kv'}, $self->filename if $self->{'kv'};
+    # print "Saved (", join(', ', keys %{$self->{'kv'}}), ") into ", $self->filename, "\n";
+};
 
 
 1;
