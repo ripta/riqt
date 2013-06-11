14c848b03809120b4cec164d13a1ba66211d326d
Ripta Pasay <rpasay@rent.com>
2013-06-11 10:17:55 -0700

    NO ISSUE: terse query placeholder
    Signed-off-by: Ripta Pasay <rpasay@rent.com>

---
+++ lib/Rent/PIQT/Plugin/TerseQuery.pm
@@ -0,0 +1,26 @@
+package Rent::PIQT::Plugin::TerseQuery;
+
+use List::Util qw/max/;
+use Moo;
+use Rent::PIQT::Util;
+
+with 'Rent::PIQT::Plugin';
+
+sub BUILD {
+    my ($self) = @_;
+
+    $self->controller->register('`', {
+        signature => "%s (relation_set) [conditions] {projections}",
+        help => qq{
+        },
+        code => sub {
+            my ($ctrl, $args) = @_;
+            my $o = $ctrl->output;
+            $o->info('TERSE (EXPANSION=1)');
+
+            return 1;
+        },
+    });
+}
+
+1;
