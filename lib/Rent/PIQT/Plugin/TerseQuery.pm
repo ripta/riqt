1cc55392e7a7396c2f3ceca685a7e5e1887d46ac
Ripta Pasay <rpasay@rent.com>
2013-07-19 17:45:09 -0700

    NO ISSUE: terse query docs
    Signed-off-by: Ripta Pasay <rpasay@rent.com>

--- lib/Rent/PIQT/Plugin/TerseQuery.pm
+++ lib/Rent/PIQT/Plugin/TerseQuery.pm
@@ -10,9 +10,50 @@ sub BUILD {
     my ($self) = @_;
 
     $self->controller->register('`', {
-        signature => "%s (relation_set) [conditions] {projections}",
-        help => qq{
-        },
+        signature => [
+            "%s <relation_set> [<conditions>] [<projections>]",
+        ],
+        help => qq/
+            Execute a Terse Query.
+
+            The <relation_set> is a set of relations, which must be marked with
+            parentheses, e.g.:
+
+                ` (phone_numbers_t)
+
+            Multiple relations may be comma-delimited:
+
+                ` (properties_t, phone_numbers_t)
+
+            Relations may be abbreviated, as long as it is not ambiguous:
+
+                ` (prop*, phone*)
+
+            The <conditions> portion is optional, and must be marked with square
+            brackets, e.g.:
+
+                ` (phone_numbers_t) [property_id = 12345]
+
+            As with relations, attributes may be abbreviated:
+
+                ` (phone*) [prop* = 12345]
+
+            Joins can be performed on specific attribute, or the attribute left out
+            entirely if the join should be performed on all attributes named the same
+            by using the cross operator "x":
+
+                ` (phone*, prop*) [property_id]
+                ` (phone*, prop*) [prop*]
+                ` (phone* x prop*)
+
+            Queries may be nested as a relation in itself:
+
+                ` ((phone*, prop*) [prop*]) [prop* > 12345]
+
+            The <projections> portion is optional, and must be marked with curly braces:
+
+                ` (prop*) {property_id, name}
+        /,
         code => sub {
             my ($ctrl, $args) = @_;
             my $o = $ctrl->output;
