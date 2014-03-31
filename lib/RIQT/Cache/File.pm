7dab63dc3f587d757b570a81adf71a23bf45ddaf
Ripta Pasay <rpasay@rent.com>
2014-03-30 17:34:30 -0700

    Support for cache backups

--- lib/RIQT/Cache/File.pm
+++ lib/RIQT/Cache/File.pm
@@ -1,6 +1,8 @@
 package RIQT::Cache::File;
 
 use Moo;
+
+use File::Copy;
 use Storable qw/nstore retrieve/;
 
 extends 'RIQT::Cache';
@@ -10,14 +12,29 @@ has 'filename' => (is => 'rw', required => 1);
 sub BUILD {
     my ($self) = @_;
 
+    # Try the regular cache file
     if (-e $self->filename) {
         $self->{'kv'} = eval { retrieve $self->filename };
+
         if ($@) {
-            warn sprintf("Cannot load cache from %s: %s\n",
-                $self->filename,
-                $@,
-            );
-            $self->{'kv'} = {};
+            # Try a backup file if one exists
+            if (-e $self->backup_filename) {
+                $self->{'kv'} = eval { retrieve $self->backup_filename };
+
+                if ($@) {
+                    warn sprintf("Cannot load cache from %s: %s\n",
+                        $self->backup_filename,
+                        $@,
+                    );
+                    $self->{'kv'} = {};
+                }
+            } else {
+                warn sprintf("Cannot load cache from %s: %s\n",
+                    $self->filename,
+                    $@,
+                );
+                $self->{'kv'} = {};
+            }
         }
     } else {
         $self->{'kv'} = {};
@@ -58,10 +75,37 @@ around POSTBUILD => sub {
 
 around save => sub {
     my ($orig, $self) = @_;
+
+    # Call the original method first, in case it needs to do clean up
     $self->$orig;
+
+    # Bail out if there's nothing to save
+    return 1 unless $self->{'kv'};
+
+    # If a config already exists, back it up first before overwriting
+    if (-e $self->filename) {
+        # Delete backup if one exists
+        if (-e $self->backup_filename) {
+            unlink $self->backup_filename or do {
+                warn "Could not unlink " . $self->backup_filename . ": $!";
+                return 0;
+            };
+        }
+
+        # Copy the original to the backup
+        unless (copy($self->filename, $self->backup_filename)) {
+            warn "Could not create a backup of " . $self->filename . ": $!";
+            return 0;
+        }
+    }
+
+    # Save the new file, overwriting if one already exists
     nstore $self->{'kv'}, $self->filename if $self->{'kv'};
-    # print "Saved (", join(', ', keys %{$self->{'kv'}}), ") into ", $self->filename, "\n";
     return 1;
 };
+
+sub backup_filename {
+    return $_[0]->filename . '.bak';
+}
 
 1;
