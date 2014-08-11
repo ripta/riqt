84b42b87136162bedb5ce516fdab956f6cb3b5c4
Ripta Pasay <rpasay@rent.com>
2014-08-10 19:30:14 -0700

    Bump version to 1.1.0

--- lib/RIQT/REPL.pm
+++ lib/RIQT/REPL.pm
@@ -10,7 +10,7 @@ use String::Escape qw/quote printable/;
 use Term::ReadLine;
 use Time::HiRes qw/gettimeofday tv_interval/;
 
-our $VERSION = '1.0.4';
+our $VERSION = '1.1.0';
 
 # Generate the 'isa' clause for some 'has' below. Given a C<$name>, this sub
 # generates an anonymous subroutine that in turn expects one argument that is
